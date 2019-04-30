06e0109725380aa4919e4de2144c9d4d606c5143
Adella <adellakenn@gmail.com>
2019-04-30 13:43:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+#   Should return "Hello, my name is #{name}." 
+#   Should not hard-code the response
+def batch_badge_creator(name)
+  badges = []
+  name.each { |string| badges << "Hello, my name is #{string}." }
+  badges
+end
+
+# puts the list of badges and room_assignemnts
+# "Hello, #{name}! You'll be assigned to room #{room_assignment}!"
+def assign_rooms(attendees)
+  hash = Hash.new 
+  
+end
