c39caf4128cf1d8c14250d76144e1af830197beb
Adella <adellakenn@gmail.com>
2019-04-30 13:58:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,6 @@ end
 # "Hello, #{attendees}! You'll be assigned to room #{room_assignment}!"
 def assign_rooms(attendees)
   hash = Hash.new 
-  attendees.each_with_index { |attendees, index| }
-   "Hello, #{attendees}! You'll be assigned to room #{index}!"
+  attendees.each_with_index { |attendees, index| puts "Hello, #{attendees}! You'll be assigned to room #{index}!" }
+   
 end 
