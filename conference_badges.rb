907ba440c3f5cef08a30bcd1fe644eeaf38a3fdf
Adella <adellakenn@gmail.com>
2019-04-30 16:15:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,6 +24,6 @@ end
 #   Should puts the list of badges and room room_assignemnts
 def printer(attendees)
   def batch_badge_creator(attendees)
-    attendees.each { |x| print x, ""}
+    batch_badge_creator(attendees).each { |x| print x, ""}
   end
 end
