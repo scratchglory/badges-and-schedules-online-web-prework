a95a2c5fb4f9dbbec670426e69093a3a530d6def
Adella <adellakenn@gmail.com>
2019-04-30 14:09:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,5 +16,6 @@ end
 # puts the list of badges and room_assignemnts
 # "Hello, #{attendees}! You'll be assigned to room #{room_assignment}!"
 def assign_rooms(attendees)
+  rooms = []
   attendees.each_with_index { |value, index| puts "Hello, #{value}! You'll be assigned to room #{index}!" }
 end 
